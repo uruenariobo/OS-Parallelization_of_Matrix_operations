701a50e932c6528150d4527a3ca7d66f4fcddb0a
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2023-04-27 17:20:35 -0500

    Add functional parallel method

--- scalar_matrix.c
+++ scalar_matrix.c
@@ -1,52 +1,44 @@
 #include <pthread.h>
-#include "matrix.h"
-#include "matrix.c"
 
-typedef struct
-{
-  const Matrix *M;
-  int rows;
-  int cols;
-  double k;
-  Matrix *R;
-  int start_row; // fila de inicio asignada a este hilo
-  int end_row;   // fila final asignada a este hilo (no procesada)
-} ScalarMatrixArgs;
+typedef struct ThreadData ThreadData;
+struct ThreadData {
+    Matrix* M;
+    double k;
+    int row_start;
+    int row_end;
+};
 
-void *scalar_matrix_thread(void *arg)
-{
-  ScalarMatrixArgs *args = (ScalarMatrixArgs *)arg;
-
-  scalar_matrix(args->M, args->end_row - args->start_row, args->cols, args->k, args->R);
-
-  return NULL;
+void* scalar_matrix_thread(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    Matrix* M = data->M;
+    double k = data->k;
+    int row_start = data->row_start;
+    int row_end = data->row_end;
+    
+    for (int i = row_start; i < row_end; ++i) {
+        for (int j = 0; j < M->cols; ++j) {
+            M->elements[i][j] *= k;
+        }
+    }
+    
+    pthread_exit(NULL);
 }
 
-// Aquí va el proceso concurrente
-void scalar_matrix_concurrent(const Matrix *M, int rows, int cols, double k, Matrix *R)
-{
-  const int NUM_THREADS = 4; // Numero de hilos
-  pthread_t threads[NUM_THREADS];
-  ScalarMatrixArgs args[NUM_THREADS];
-
-  int rows_per_thread = rows / NUM_THREADS;
-  int last_thread_rows = rows_per_thread + rows % NUM_THREADS;
-
-  for (int t = 0; t < NUM_THREADS; ++t)
-  {
-    args[t].M = M;
-    args[t].rows = rows;
-    args[t].cols = cols;
-    args[t].k = k;
-    args[t].R = R;
-    args[t].start_row = t * rows_per_thread;
-    args[t].end_row = args[t].start_row + rows_per_thread;
-
-    pthread_create(&threads[t], NULL, scalar_matrix_thread, &args[t]);
-  }
-
-  for (int t = 0; t < NUM_THREADS; ++t)
-  {
-    pthread_join(threads[t], NULL);
-  }
+void scalar_matrix_concurrent(Matrix* M, double k) {
+    const int num_threads = M->rows;
+    pthread_t threads[num_threads];
+    ThreadData thread_data[num_threads];
+    
+    for (int i = 0; i < num_threads; ++i) {
+        thread_data[i].M = M;
+        thread_data[i].k = k;
+        thread_data[i].row_start = i;
+        thread_data[i].row_end = i + 1;
+        
+        pthread_create(&threads[i], NULL, scalar_matrix_thread, &thread_data[i]);
+    }
+    
+    for (int i = 0; i < num_threads; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 }
